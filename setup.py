c4aa7ddfa6a2658ddd049e9afdb614e5fc2706cc
Soeren Wegener <wegener92@gmail.com>
2018-10-26 01:49:06 +0200

    Add travis.yml

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup
 setup(
     name='seatfinder',
     version='0.0.1',
+    url='https://github.com/soerface/py_seatfinder',
     description='Easy access to the data of the KIT seatfinder. '
                 'This project is not affiliated in any way with the original author of https://www.seatfinder.de/',
     packages=['seatfinder'],
-    dependencies=['requests', 'pandas']
+    dependencies=['requests', 'pandas'],
+    tests_require=['nose'],
+    test_suite='nose.collector',
 )
